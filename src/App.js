c819e56d626abcf91c3d2fc493525a72f41605ac
Hoang Hai <76964646+hoanghai9650@users.noreply.github.com>
2021-03-28 21:49:14 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import BT3ThucHanhLayout from './BaiTapThucHanhLayout/BT3ThucHanhLayout';
+import BaiTapChonXe from './Components/BaiTapChonXe/BaiTapChonXe';
+import BaiTapLayout from './Components/BaiTaplayout/BaiTapLayout';
+import Databinding from './Databinding/Databinding';
+import HandleEvent from './HandleEvent/HandleEvent';
+import logo from './logo.svg';
+import RenderWithMap from './RenderWithMap/RenderWithMap';
+import StateDemo from './StateDemo/StateDemo';
+import StyleComponent from './StyleComponent/StyleComponent';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <BaiTapLayout /> */}
+      {/* <StyleComponent /> */}
+      {/* <Databinding /> */}
+      {/* <HandleEvent /> */}
+      {/* <StateDemo /> */}
+      {/* <BaiTapChonXe /> */}
+      {/* <RenderWithMap /> */}
+      <BT3ThucHanhLayout />
+    </div>
+
+  );
+}
+
+export default App;
